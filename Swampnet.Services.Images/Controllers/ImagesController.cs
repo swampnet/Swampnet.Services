b6d79007538796c0446556794a60255e83ba6916
aime-johnson <aime.johnson@theswamp.co.uk>
2017-11-16 16:10:12 +0000

    Add image GetDetails

--- Swampnet.Services.Images/Controllers/ImagesController.cs
+++ Swampnet.Services.Images/Controllers/ImagesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Serilog;
+using Swampnet.Services.Images.Entities;
 using Swampnet.Services.Images.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,14 @@ using System.Threading.Tasks;
 
 namespace Swampnet.Services.Images.Controllers
 {
+    /*
+     * Get image by id
+     * Get image history
+     * Get specific image version
+     * Add image
+     * Update image
+     * 
+     */
     public class ImagesController : Controller
     { 
         private readonly IImagesRepository _images;
@@ -57,6 +66,30 @@ namespace Swampnet.Services.Images.Controllers
                 return File("~/Images/Logo.png", "image/png");
                 //var image = System.IO.File.OpenRead("C:\\test\random_image.jpeg");
                 //return File(image, "image/jpeg");
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, ex.Message);
+
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex);
+            }
+        }
+
+
+        [HttpGet("api/images/{id}/details")]
+        public async Task<IActionResult> GetDetails(string id)
+        {
+            try
+            {
+                await Task.CompletedTask; // HACK
+
+                Log.Information("GetDetails: {id}", id);
+
+                return Ok(new ImageDetails()
+                {
+                    Id = id,
+                    Name = $"Image {id}"
+                });
             }
             catch (Exception ex)
             {
---
+++ Swampnet.Services.Images/Entities/ImageDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Swampnet.Services.Images.Entities
+{
+    /*
+     * Keep in mind that an image itself contains meta-data about itself, and we should be using *that* as the
+     * canonical source for some of this stuff.
+     * 
+     * Store in ImageDetails if we need to represent it in the database
+     */
+    public class ImageDetails
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
